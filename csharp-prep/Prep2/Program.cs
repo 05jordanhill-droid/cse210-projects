3a6dbb32aa6c809a7b3df297bb5f7c306e21a41a
05jordanhill-droid <05jordanhill@gmail.com>
2026-04-23 18:03:36 -0600

    w01_prove_assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,53 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string jahUserInputGradePercentage = Console.ReadLine();
+        float jahGradePercentage = float.Parse(jahUserInputGradePercentage);
+
+        string jahGradeLetter = "";
+        string jahGradeSign = "";
+
+        if (jahGradePercentage >= 90)
+        {
+            jahGradeLetter = "A";
+        }
+        else if (jahGradePercentage >= 80)
+        {
+            jahGradeLetter = "B";
+        }
+        else if (jahGradePercentage >= 70)
+        {
+            jahGradeLetter = "C";
+        }
+        else if (jahGradePercentage >= 60)
+        {
+            jahGradeLetter = "D";
+        }
+        else if (jahGradePercentage < 60)
+        {
+            jahGradeLetter = "F";
+        }
+
+        if (jahGradePercentage % 10 >= 7 && jahGradeLetter != "A" && jahGradeLetter != "F")
+        {
+            jahGradeSign = "+";
+        }
+        else if (jahGradePercentage % 10 < 3 && jahGradeLetter != "F")
+        {
+            jahGradeSign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {jahGradeLetter}{jahGradeSign}.");
+
+        if (jahGradePercentage < 60)
+        {
+            Console.WriteLine("You failed. Better luck next time!");
+        }
+        else if (jahGradePercentage >= 60)
+        {
+            Console.WriteLine("You passed.");
+        }
     }
 }
