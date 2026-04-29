fdd3681656fc7f672260535f473488f91253e378
05jordanhill-droid <05jordanhill@gmail.com>
2026-04-28 20:01:19 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int jah_number = randomGenerator.Next(1, 101);
+            int jah_guess;
+
+            int jah_counter = 0;
+
+            Console.WriteLine(jah_number);
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string jah_raw_guess = Console.ReadLine();
+                jah_guess = int.Parse(jah_raw_guess);
+                
+                if (jah_guess > jah_number)
+                {
+                    Console.WriteLine("Too high. Guess lower.");
+                }
+                else if (jah_guess < jah_number)
+                {
+                    Console.WriteLine("Too low. Guess higher.");
+                }
+
+                jah_counter ++;
+            } while(jah_guess != jah_number);
+
+            Console.WriteLine($"You guessed it in {jah_counter} attempts. Good job!");
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+        }while (playAgain == "yes");
+
     }
 }
