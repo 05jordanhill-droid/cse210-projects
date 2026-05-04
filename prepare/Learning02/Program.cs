265605baf3df3aeff07089277bf178f421e3aed6
05jordanhill-droid <05jordanhill@gmail.com>
2026-05-04 09:55:47 -0600

    w02 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,25 +6,25 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
 
-        Job job1 = new Job();
-        job1._company = "Microsoft";
-        job1._jobTitle = "Engineer";
-        job1._startYear = 2000;
-        job1._endYear = 2015;
+        Job jahJob1 = new Job();
+        jahJob1._company = "Microsoft";
+        jahJob1._jobTitle = "Engineer";
+        jahJob1._startYear = 2000;
+        jahJob1._endYear = 2015;
 
-        Job job2 = new Job();
-        job2._company = "Google";
-        job2._jobTitle = "HR";
-        job2._startYear = 2005;
-        job2._endYear = 2007;
+        Job jahJob2 = new Job();
+        jahJob2._company = "Google";
+        jahJob2._jobTitle = "HR";
+        jahJob2._startYear = 2005;
+        jahJob2._endYear = 2007;
 
-        Resume jobs = new Resume();
+        Resume jahJobs = new Resume();
 
-        jobs._name = "Jordan Hill";
+        jahJobs._name = "Jordan Hill";
 
-        jobs._jobs.Add(job1);
-        jobs._jobs.Add(job2);
+        jahJobs._jobs.Add(jahJob1);
+        jahJobs._jobs.Add(jahJob2);
         
-        jobs.Display();
+        jahJobs.Display();
     }
 }
